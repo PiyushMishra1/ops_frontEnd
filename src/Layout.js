88de1256226f9275ec0082794d9cb28f6158eff5
piyush mishra <piyushmishra967@gmail.com>
2024-08-22 23:29:58 +0530

    temp fix for layout side bar and Dashboard

--- src/Layout.js
+++ src/Layout.js
@@ -2,35 +2,8 @@ import React, { useState } from "react";
 import Sidebar from "./components/Sidebar";
 import Header from "./components/Header";
 
-const Layout = ({ children, isAuthenticated }) => {
-  const [sidebarOpen, setSidebarOpen] = useState(true);
-
-  const toggleSidebar = () => {
-    setSidebarOpen(!sidebarOpen);
-  };
-  if (!isAuthenticated) {
-    return <>{children}</>;
-  }
-  return (
-    <div className="flex h-screen  bg-gray-100 overflow-hidden">
-      {/* Overlay for mobile */}
-      {sidebarOpen && (
-        <div
-          className="fixed inset-0 z-20 bg-black bg-opacity-50 lg:hidden"
-          onClick={toggleSidebar}
-        ></div>
-      )}
-
-      {/* Sidebar */}
-      <Sidebar sidebarOpen={sidebarOpen} toggleSidebar={toggleSidebar} />
-
-      {/* Main content */}
-      <div className={`flex flex-col flex-1 w-full min-w-0 `}>
-        <Header toggleSidebar={toggleSidebar} sidebarOpen={sidebarOpen} />
-        <main className="">{children}</main>
-      </div>
-    </div>
-  );
+const Layout = ({ children }) => {
+  return <main className="">{children}</main>;
 };
 
 export default Layout;
