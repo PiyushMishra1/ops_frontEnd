351e45f2fcd8b586b202872fc11ee496c8081c67
piyush mishra <piyushmishra967@gmail.com>
2024-08-22 23:35:58 +0530

    some fixes

--- src/Layout.js
+++ src/Layout.js
@@ -1,6 +1,4 @@
 import React, { useState } from "react";
-import Sidebar from "./components/Sidebar";
-import Header from "./components/Header";
 
 const Layout = ({ children }) => {
   return <main className="">{children}</main>;
